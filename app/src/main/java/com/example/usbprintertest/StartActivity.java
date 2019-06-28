ce7b9a98a55b5f2f81cb7e88ba4df5e3e43fca8a
devallever <devallever@163.com>
2019-06-28 16:31:42 +0800

    init.

---
+++ app/src/main/java/com/example/usbprintertest/StartActivity.java
@@ -0,0 +1,27 @@
+package com.example.usbprintertest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class StartActivity extends Activity implements OnClickListener{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+		Button openPrint = (Button)findViewById(R.id.OpenPrintTask_btn);
+		openPrint.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		if(view.getId()==R.id.OpenPrintTask_btn){
+			Intent intent = new Intent(StartActivity.this,MainActivity.class);
+			startActivity(intent);
+		}
+		
+	}
+}
---
+++ app/src/main/java/com/tcinterlib/lotteryprinterusb/MainActivity.kt
@@ -0,0 +1,12 @@
+//package com.tcinterlib.lotteryprinterusb
+//
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(.layout.activity_main)
+//    }
+//}
